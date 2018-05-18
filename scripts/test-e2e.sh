0a407a09faed925daa566b17fecd62d06247879c
arekkas <aeneas@ory.am>
2018-05-18 17:05:00 +0200

    consent: Properly handles no result errors from consent check

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -5,7 +5,14 @@ set -euo pipefail
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 DATABASE_URL=memory hydra serve --dangerous-force-http --disable-telemetry &
+HYDRA_URL=http://localhost:4444 PORT=3000 LOGIN=accept CONSENT=accept mock-lcp &
+HYDRA_URL=http://localhost:4444 PORT=3001 LOGIN=accept CONSENT=reject mock-lcp &
+HYDRA_URL=http://localhost:4444 PORT=3002 LOGIN=reject CONSENT=reject mock-lcp &
+
 while ! echo exit | nc 127.0.0.1 4444; do sleep 1; done
+while ! echo exit | nc 127.0.0.1 3000; do sleep 1; done
+while ! echo exit | nc 127.0.0.1 3001; do sleep 1; done
+while ! echo exit | nc 127.0.0.1 3002; do sleep 1; done
 
 export HYDRA_URL=http://localhost:4444/
 export OAUTH2_CLIENT_ID=foobar
@@ -15,3 +22,22 @@ hydra clients create --id $OAUTH2_CLIENT_ID --secret $OAUTH2_CLIENT_SECRET -g cl
 token=$(hydra token client)
 hydra token introspect $token
 hydra clients delete foobar
+
+hydra clients create \
+    --endpoint http://localhost:4444 \
+    --id test-client \
+    --secret test-secret \
+    --response-types code,id_token \
+    --grant-types refresh_token,authorization_code \
+    --scope openid,offline \
+    --callbacks http://127.0.0.1:4445/callback
+
+hydra token user \
+    --endpoint http://localhost:4444/ \
+    --scope openid,offline \
+    --client-id test-client \
+    --client-secret test-secret &
+
+sleep 5
+
+curl http://127.0.0.1:4445
